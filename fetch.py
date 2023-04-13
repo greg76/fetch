4a4adb05b9252b1cc42a615f0eadc3d470941cb4
Greg Dervarics <gergely.dervarics@nexthink.com>
2023-04-13 16:01:40 +0200

    add: preliminary cache size management

--- fetch.py
+++ fetch.py
@@ -8,6 +8,7 @@ from sys import version_info
 
 
 expiry_seconds: int = 600
+cache_size_in_MB: float = 5
 
 __tmp_dir = os.path.join(tempfile.gettempdir(), "fetchpy-cache")
 __default_user_agent = f"python{version_info.major}.{version_info.minor} urllib"
@@ -71,16 +72,41 @@ def fetch(url: str, headers: dict = None, auth: tuple = None) -> str:
     return content
 
 
-def cache_usage() -> int:
-    file_sizes = (
-        os.path.getsize(os.path.join(__tmp_dir, filename))
+def cache_list() -> list:
+    details = [
+        {
+            "path":     os.path.join(__tmp_dir, filename),
+            "size":     os.path.getsize(os.path.join(__tmp_dir, filename)),
+            "updated":  max(
+                            os.path.getctime(os.path.join(__tmp_dir, filename)),
+                            os.path.getmtime(os.path.join(__tmp_dir, filename))
+                        )
+        }
         for filename in os.listdir(__tmp_dir)
         if os.path.isfile(os.path.join(__tmp_dir, filename))
-    )
+    ]
+    details.sort(key = lambda x: x["updated"])
+    return details
+
+
+def free_cache():
+    to_be_freed = cache_usage() - cache_size_in_MB * 10**6
+    if to_be_freed > 0:
+        for file in cache_list():
+            os.remove(file["path"])
+            to_be_freed -= file["size"]
+            if to_be_freed <= 0:
+                break
+
+
+def cache_usage() -> int:
+    file_sizes = (file["size"] for file in cache_list())
     return sum(file_sizes)
 
 
 if __name__ == '__main__':
+    print(f'cache location: "{__tmp_dir}"')
+    free_cache()
+    print(f"cache size: {cache_usage():,} / {cache_size_in_MB * 10**6:,.0f} bytes")
+    print(f'user agent: "{__default_user_agent}"')
 
-    print(f"current cache size: {cache_usage():,} bytes")
-    print(f"user agent: {__default_user_agent}")
